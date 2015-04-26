61a31287f5af2c7731b3154f9bdfae73fd0b0afc
DanielAguirre <danter1@gmail.com>
2015-04-26 10:41:39 -0500

    Se creo el elemento simpleList en el archivo reactApp.jsx

---
+++ reactApp.jsx
@@ -0,0 +1,12 @@
+'use strict'
+
+var SimpleListRow = React.createClass({
+    render: function(){
+        var rows = this.props.simpleList;
+        return (
+            <ol>
+                <li>{elemt.row}</li>
+            </lo>
+        )
+    }
+})
