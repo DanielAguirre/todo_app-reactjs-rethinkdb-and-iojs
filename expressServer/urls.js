aa295aa549dcbeb29ee03ea682861cb181e2ca29
DanielAguirre <danter1@gmail.com>
2015-04-28 22:27:21 -0500

    Se Creo la ruta 'get' simplelist que regresa un arreglo de 'rows' con los pasos a seguir para dominar un lenguage de programacion

--- expressServer/urls.js
+++ expressServer/urls.js
@@ -8,5 +8,23 @@ router.get('',function(req,res){
 	res.render('index',{})
 })
 
+router.get('/simpleList',function(req,res){
+	res
+		.json([
+			{
+        		'row': 'Leer la documentación'
+    		},
+    		{
+        		'row': 'Completar Tutoriales'
+    		},
+    		{
+        		'row':'Crear un Demo'
+    		},
+    		{
+        		'row':'Escribir sobre lo que aprendiste'
+    		}
+    	])
+})
+
 
 module.exports = router
--- public/js/reactApp.js
+++ public/js/reactApp.js
@@ -1,59 +1,54 @@
+var SimpleList = React.createClass({displayName: "SimpleList",
+    getInitialState: function(){
+        console.log('initial')
+        return {
+            simpleList: [
+                {
+                    'row':'cargando   ....'
+                }
+            ]
+        }
+    },
+    componentDidMount: function(){
+        $.ajax({
+            url:this.props.url,
+            dataType: 'json',
+            success: function(data){
+                this.setState({simpleList:data})
+            }.bind(this),
+            error:function(xhr,status,err){
+                console.error(this.props.url,status,err.toString())
+            }.bind(this)
+        })
+    },
+    render: function(){
+        return (
+            React.createElement("span", null, 
+                React.createElement("p", null, React.createElement("strong", null, "Pasos para dominar un nuevo lenguage de programación")), 
+                React.createElement(SimpleListRow, {simpleList: this.state.simpleList})
+             )
+        )
+    }
+})
 
-
-
-(function(){
-    console.log("react")
-    var SimpleList = React.createClass({displayName: "SimpleList",
-        getInitialState: function(){
-            console.log('initial')
-            return {
-                simpleList: [
-                    {
-                        'row':'cargando   ....'
-                    }
-                ]
-            }
-        },
-        componentDidMount: function(){
-            $.ajax({
-                url:this.props.url,
-                dataType: 'json',
-                success: function(data){
-                    this.setState({simpleList:data})
-                }.bind(this),
-                error:function(xhr,status,err){
-                    console.error(this.props.url,status,err.toString())
-                }.bind(this)
-            })
-        },
-        render: function(){
-            return (
-                React.createElement("span", null, 
-                    React.createElement("p", null, React.createElement("strong", null, "Pasos para dominar un nuevo lenguage de programación")), 
-                    React.createElement(SimpleListRow, {simpleList: this.state.simpleList})
-                 )
+var SimpleListRow = React.createClass({displayName: "SimpleListRow",
+    render: function(){
+        var rows = this.props.simpleList;
+        console.log(this.props)
+        return (
+            React.createElement("ol", null, 
+                rows.map(function(element) {
+                    return (
+                        React.createElement("li", null, element.row)
+                    );
+                })
             )
-        }
-    })
+        );
+    }
+})
 
-    var SimpleListRow = React.createClass({displayName: "SimpleListRow",
-        render: function(){
-            var rows = this.props.simpleList;
-            return (
-                React.createElement("ol", null, 
-                    rows.map(function(element) {
-                        return (
-                            React.createElement("li", null, element.row)
-                        );
-                    })
-                )
-            );
-        }
-    })
+React.render(
+    React.createElement(SimpleList, {url: "simpleList"}),
+    document.getElementById('simpleList')
+)
 
-    React.render(
-        React.createElement(SimpleList, {url: "simpleList_data.json"}),
-        document.getElementById('simpleList')
-    )
-
-}())
